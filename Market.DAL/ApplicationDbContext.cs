911941b554e93f70091c7830e52a9dd3bf4795ce
Skalerr <46483879+skalerr@users.noreply.github.com>
2022-11-10 00:34:18 +0500

    добавил вьшные реализации, и удаление и создание заказов а надо было оказывается итемы заказов

--- Market.DAL/ApplicationDbContext.cs
+++ Market.DAL/ApplicationDbContext.cs
@@ -7,10 +7,20 @@ public sealed class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        Database.EnsureCreated();
+        // Database.EnsureDeleted();
+        // Database.EnsureCreated();
     }
     
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
     public DbSet<Provider> Providers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Provider>().HasData(
+            new Provider { Id = 1, Name = "Tom"},
+            new Provider { Id = 2, Name = "Alice"},
+            new Provider { Id = 3, Name = "Sam"}
+        );
+    }
 }
--- Market/Controllers/HomeController.cs
+++ Market/Controllers/HomeController.cs
@@ -9,23 +9,88 @@ public class HomeController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly IOrderService _orderService;
     private readonly IOrderItemService _orderItemService;
+    private readonly ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger, IOrderService orderService, IOrderItemService orderItemService)
+    public HomeController(ILogger<HomeController> logger, IOrderService orderService, IOrderItemService orderItemService, ApplicationDbContext db)
     {
         _logger = logger;
         _orderService = orderService;
         _orderItemService = orderItemService;
+        _db = db;
     }
-
-    public IActionResult Index()
-    {
-        ViewBag.Orders = _orderService.GetOrders();
-        return View();
-    }
-
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+
+    public IActionResult Index()
+    {
+        var resp = _orderService.GetOrders();
+        ViewBag.Orders  = resp.Result.Data;
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateOrder()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult CreateOrder(Order order)
+    {
+        var resp= _orderService.CreateOrder(order);
+        if (resp.Result.StatusCode == Domain.Enum.StatusCode.Ok)
+        {
+            return RedirectToAction("Index");
+        }
+        return View(order);
+
+    }
+    
+    public IActionResult EditOrder(int id)
+    {
+        var order = _orderService.GetOrder(id).Result.Data;
+        return View(order);
+    }
+    
+    [HttpPost]
+    public IActionResult EditOrder(Order order)
+    {
+        var resp = _orderService.UpdateOrder(order);
+        if (resp.Result.StatusCode == Domain.Enum.StatusCode.Ok)
+        {
+            RedirectToAction("Index");
+        }
+        return View(order);
+    }
+
+    public IActionResult Providers()
+    {
+        var model = _db.Providers.ToList();
+        return View(model);
+    }
+
+    public IActionResult OrderInfo(int id)
+    {
+        var resp = _orderService.GetOrder(id);
+        if (resp.Result.StatusCode == Domain.Enum.StatusCode.Ok)
+        {
+            return View(resp.Result.Data);
+        }
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult DeleteOrder(int id)
+    {
+        var order = _orderService.GetOrder(id).Result.Data;
+        var resp = _orderService.Delete(order);
+        if (resp.Result.StatusCode == Domain.Enum.StatusCode.Ok)
+        {
+            return RedirectToAction("Index");
+        } 
+        return RedirectToAction("OrderInfo", new { id});
     }
 }
